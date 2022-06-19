3e3b8e4d6db1e2276bba6a57f597ab3e862bdcf6
dongl <dl978916237@163.com>
2022-06-19 16:19:07 +0800

    java 代码规范示例

---
+++ code-standards/src/main/java/com/dongl/codestandards/CodeStandardsApplication.java
@@ -0,0 +1,13 @@
+package com.dongl.codestandards;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeStandardsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodeStandardsApplication.class, args);
+    }
+
+}
---
+++ code-standards/src/test/java/com/dongl/codestandards/CodeStandardsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dongl.codestandards;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeStandardsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
